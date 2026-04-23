11775efc5352970877ffe79ac194785a7949ce02
Rune Keena <runekeena@gamil.com>
2026-04-23 19:57:45 +0200

    Env vars - handle _CERT_BASE_PATH is None

--- python/src/kombit_client/configuration.py
+++ python/src/kombit_client/configuration.py
@@ -4,14 +4,14 @@ import os
 CVR_NUMBER = os.environ.get('CVR_NUMBER')
 
 # Certificates
-_CERT_BASE_PATH = os.environ.get('CERT_BASE_PATH')
+_CERT_BASE_PATH = os.environ.get('CERT_BASE_PATH', None)
 CLIENT_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ['CLIENT_CERT']) if (_CERT_BASE_PATH and (env := os.environ.get('CLIENT_CERT'))) else None
-CLIENT_CERT_BASE64 = os.environ.get('CLIENT_CERT_BASE64', None)
-CLIENT_CERT_PASS = os.environ.get('CLIENT_CERT_PASS', None)
-ROOT_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('ROOT_CERT', 'CA-Den Danske Stat OCES rod-CA.cer'))
-ACCESS_CONTROL_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('ACCESS_CONTROL_CERT', 'ADG_PROD_Adgangsstyring_2.cer'))
-SIGNING_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('SIGNING_CERT', 'new_SP_PROD_Signing_1.cer'))
-YDELSESINDEKS_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('YDELSESINDEKS_CERT', 'YDI_PROD_Ydelsesindeks_2.cer'))
+CLIENT_CERT_BASE64 = os.environ.get('CLIENT_CERT_BASE64', None) if _CERT_BASE_PATH else None
+CLIENT_CERT_PASS = os.environ.get('CLIENT_CERT_PASS', None) if _CERT_BASE_PATH else None
+ROOT_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('ROOT_CERT', 'CA-Den Danske Stat OCES rod-CA.cer')) if _CERT_BASE_PATH else None
+ACCESS_CONTROL_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('ACCESS_CONTROL_CERT', 'ADG_PROD_Adgangsstyring_2.cer')) if _CERT_BASE_PATH else None
+SIGNING_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('SIGNING_CERT', 'new_SP_PROD_Signing_1.cer')) if _CERT_BASE_PATH else None
+YDELSESINDEKS_CERT_PATH = os.path.join(_CERT_BASE_PATH, os.environ.get('YDELSESINDEKS_CERT', 'YDI_PROD_Ydelsesindeks_2.cer')) if _CERT_BASE_PATH else None
 
 # Endpoints
 STS_ENDPOINT_ADDRESS = os.environ.get('STS_ENDPOINT_ADDRESS', 'https://n2adgangsstyring.stoettesystemerne.dk/runtime/services/kombittrust/14/certificatemixed')
